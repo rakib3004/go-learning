bb566c084efcc43ae3f88ece8b39216139e7c3ce
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-11-07 10:29:50 +0600

    add Main.java

---
+++ Paint/edu/cmu/hcii/paint/Main.java
@@ -0,0 +1,17 @@
+package edu.cmu.hcii.paint;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        PaintWindow paintWindow = new PaintWindow(800, 600); // Adjust the initialWidth and initialHeight as needed
+        paintWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
